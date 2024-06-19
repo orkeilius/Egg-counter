5af5c4722e987b3f3ac74295ef1210c73eb28bc8
orkeilius <orkeilius@users.noreply.github.com>
2024-06-19 17:08:12 +0200

    add getter for daySaleRepository

--- app/src/main/java/com/lesfeesdesoeufsbio/eggcounter/model/DaySaleRepository.kt
+++ app/src/main/java/com/lesfeesdesoeufsbio/eggcounter/model/DaySaleRepository.kt
@@ -1,6 +1,8 @@
 package com.lesfeesdesoeufsbio.eggcounter.model
 
 import android.content.Context
+import android.content.Context.MODE_PRIVATE
+import kotlinx.datetime.LocalDate
 import kotlinx.serialization.json.Json
 import kotlinx.serialization.encodeToString
 import java.io.File
@@ -12,7 +14,7 @@ class DaySaleReposytory private constructor(val fileDir: File) {
 
         fun getInstance(context: Context): DaySaleReposytory {
             if (this.instance == null) {
-                this.instance = DaySaleReposytory(context.filesDir)
+                this.instance = DaySaleReposytory(context.getDir("sale",MODE_PRIVATE))
             }
             return this.instance!!
         }
@@ -20,12 +22,38 @@ class DaySaleReposytory private constructor(val fileDir: File) {
 
 
       fun save(data : DaySale){
-        val fileName = "DaySale-" + data.date.toString()
+        val fileName = generateFileName(data.date)
         val file = File(fileDir, fileName)
 
         if (file.exists() && file.isFile) {
             file.delete()
         }
         file.writeText(Json.encodeToString(data))
+    }
+
+    fun getAllDay(): ArrayList<DaySale>{
+        val files = fileDir.listFiles()?.filter { it.isFile }
+        var out = arrayListOf<DaySale>()
+
+        files?.forEach{ file ->
+            out.add(Json.decodeFromString<DaySale>(file.readText()))
+        }
+        return out
+    }
+
+    fun getDay(date : LocalDate) : DaySale{
+        val file = File(fileDir,generateFileName(date))
+
+        if(!file.exists()){
+            return DaySale(date)
+        }
+        return Json.decodeFromString<DaySale>(file.readText())
+    }
+
+
+
+
+    private fun generateFileName(date : LocalDate): String{
+        return "DaySale-" + date.toString()
     }
 }
