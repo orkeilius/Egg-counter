1bb2e91f933ceadae229f3d97aef9a3c8e25b518
orkeilius <orkeilius@users.noreply.github.com>
2024-06-30 10:47:37 +0200

    move mainView to is own file

--- app/src/main/java/com/lesfeesdesoeufsbio/eggcounter/view/MainActivity.kt
+++ app/src/main/java/com/lesfeesdesoeufsbio/eggcounter/view/MainActivity.kt
@@ -4,11 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.material.icons.Icons
@@ -20,25 +16,17 @@ import androidx.compose.material3.NavigationBarItem
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.vector.ImageVector
-import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import androidx.lifecycle.ProcessLifecycleOwner
-import androidx.lifecycle.viewmodel.compose.viewModel import androidx.navigation.NavHostController
+import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.currentBackStackEntryAsState
 import androidx.navigation.compose.rememberNavController
-import com.lesfeesdesoeufsbio.eggcounter.model.EggNumber
-import com.lesfeesdesoeufsbio.eggcounter.model.EggSize
 import com.lesfeesdesoeufsbio.eggcounter.utils.AppLifecycleObserver
-import com.lesfeesdesoeufsbio.eggcounter.view.item.CounterItem
 import com.lesfeesdesoeufsbio.eggcounter.view.ui.theme.EggCounterTheme
-import com.lesfeesdesoeufsbio.eggcounter.viewModel.MainViewModel
 
 
 class MainActivity : ComponentActivity() {
@@ -98,41 +86,6 @@ fun Router(navController: NavHostController = rememberNavController()){
 
         }
 
-
-    }
-}
-
-@Composable
-fun MainView(modifier: Modifier = Modifier,mainViewModel: MainViewModel = viewModel()) {
-    val daySale by mainViewModel.daySale.collectAsState()
-    Column(
-        modifier = modifier
-            .fillMaxSize(),
-        verticalArrangement = Arrangement.Center
-        ,
-    ){
-        for (quantity in EggNumber.values()){
-            Row(
-                modifier = Modifier
-                    .wrapContentHeight()
-                    .weight(1f)
-            ) {
-                for (size in EggSize.values()){
-                    CounterItem(quantity,size,
-                        Modifier
-                            .weight(0.25f)
-                            .padding(6.dp))
-                }
-            }
-
-        }
-        Text(
-            text = "Total : %.2f €".format(daySale.getTotal()),
-            fontSize = 34.sp,
-            textAlign = TextAlign.Center
-            , modifier = Modifier.fillMaxWidth()
-
-        )
 
     }
 }
---
+++ app/src/main/java/com/lesfeesdesoeufsbio/eggcounter/view/MainView.kt
@@ -0,0 +1,58 @@
+package com.lesfeesdesoeufsbio.eggcounter.view
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.wrapContentHeight
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.lesfeesdesoeufsbio.eggcounter.model.EggNumber
+import com.lesfeesdesoeufsbio.eggcounter.model.EggSize
+import com.lesfeesdesoeufsbio.eggcounter.view.item.CounterItem
+import com.lesfeesdesoeufsbio.eggcounter.viewModel.MainViewModel
+
+@Composable
+fun MainView(modifier: Modifier = Modifier, mainViewModel: MainViewModel = viewModel()) {
+    val daySale by mainViewModel.daySale.collectAsState()
+    Column(
+        modifier = modifier
+            .fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+    ) {
+        for (quantity in EggNumber.values()) {
+            Row(
+                modifier = Modifier
+                    .wrapContentHeight()
+                    .weight(1f)
+            ) {
+                for (size in EggSize.values()) {
+                    CounterItem(
+                        quantity, size,
+                        Modifier
+                            .weight(0.25f)
+                            .padding(6.dp)
+                    )
+                }
+            }
+
+        }
+        Text(
+            text = "Total : %.2f €".format(daySale.getTotal()),
+            fontSize = 34.sp,
+            textAlign = TextAlign.Center,
+            modifier = Modifier.fillMaxWidth()
+
+        )
+
+    }
+}
